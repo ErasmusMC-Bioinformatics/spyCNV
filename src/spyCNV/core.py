d09199677b8a48cfdfdd429266632e400d7dd179
Dennis Dollée <no.ploidy@pm.me>
2026-02-18 16:22:44 +0100

    Add html rendering with embedding javascript

--- src/spyCNV/core.py
+++ src/spyCNV/core.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
+from importlib.resources import files
+
 from jinja2 import Environment, PackageLoader
+
+_app = "spyCNV"
+_pkg = files(_app)
 
 
 def read_file(file: str):
@@ -13,9 +18,38 @@ def write_file(file: str, content: str):
         f.write(content)
 
 
-def render(sample_id: str, spec: str) -> str:
+def _load_resource(path_parts: list[str]) -> str:
+    content = (_pkg.joinpath(*path_parts)).read_text()
+    return content
 
-    env = Environment(loader=PackageLoader("spyCNV", "templates"))
+
+def render_html(sample_id: str, genome: str = "hg19") -> str:
+    """Render html with embedded javascript."""
+    env = Environment(loader=PackageLoader(_app, "templates"))
     template = env.get_template("base.html.jinja2")
-    html = template.render(sample_id=sample_id, spec=spec)
+
+    genomespy_js = _load_resource(["static", "genome-spy_core@0.70.0.js"])
+    plots_js = (
+        _load_resource(["plots", "ideogramTrack.js"])
+        + _load_resource(["plots", "geneAnnotationTrack.js"]),
+    )
+    cytoband_data = _load_resource(["data", f"cytoBand.{genome}.tsv"])
+    refseq_gene_data = _load_resource(
+        ["data", f"refSeq_genes_scored_compressed.{genome}.tsv"]
+    )
+
+    spec = {
+        "genome": {"name": "hg19"},
+        "vconcat": [
+            f'ideogramTrack("{cytoband_data}")',
+            f'geneAnnotationTrack("{refseq_gene_data}")',
+        ],
+    }
+
+    html = template.render(
+        sample_id=sample_id,
+        genomespy_js=genomespy_js,
+        plots_js=plots_js,
+        spec=spec,
+    )
     return html
--- tests/spyCNV/test_core.py
+++ tests/spyCNV/test_core.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
-import json
 import os
 
-from spyCNV.core import render, write_file
+from spyCNV.core import _load_resource, render_html, write_file
 
 
 def test_render():
-    sample_id = "M25-XXXXT"
+    sample_id = "TEST-sample"
     output_path = f"{sample_id}.html"
-    html_content = render(sample_id, json.dumps({1: "one", 2: "two"}))
+    html_content = render_html(sample_id, "hg19")
     write_file(output_path, html_content)
 
     assert os.path.exists(output_path)
     os.unlink(output_path)
     assert not os.path.exists(output_path)
+
+
+def test_load_resource():
+    plot_ideomTrack = _load_resource(["plots", "ideogramTrack.js"])
+    gs_0_70 = _load_resource(["static", "genome-spy_core@0.70.0.js"])
+
+    assert len(plot_ideomTrack) == 3504
+    assert len(gs_0_70) == 777136
