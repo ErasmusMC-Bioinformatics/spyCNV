8c1f2692be01f48ae0b738fc2a4798608e71306d
Dennis Dollée <no.ploidy@pm.me>
2026-03-05 10:31:06 +0100

    Add test files + data loader

--- tests/spyCNV/io/test_loaders.py
+++ tests/spyCNV/io/test_loaders.py
@@ -4,18 +4,17 @@ from pathlib import Path
 from spyCNV.io.loaders import (
     CNVRecord,
     _open_file,
+    create_cnv_data,
     load_input,
     load_resource,
     parse_generic_tsv,
     parse_tn,
     parse_vcf,
-    create_cnv_data,
 )
 
-BAF_TSV = Path("tests/data/MXX-ABCDT_bAllele.tsv")
-BAF_CNV = Path("tests/data/MXX-ABCDT.cnv.vcf.gz")
-TN_TSV = Path("tests/data/MXX-ABCDT.tn.tsv.gz")
-HARDFILTERED_VCF = Path("tests/data/MXX-ABCDT.hard-filtered.vcf")
+BAF_TSV = Path("tests/data/SXX-XXXT_bAllele.tsv")
+TN_TSV = Path("tests/data/SXX-XXXT.tn.tsv.gz")
+HARDFILTERED_VCF = Path("tests/data/SXX-XXXT.hard-filtered.vcf.gz")
 
 
 # def test_load_resource():
@@ -28,7 +27,7 @@ HARDFILTERED_VCF = Path("tests/data/MXX-ABCDT.hard-filtered.vcf")
 
 def test_open_file():
     with _open_file(BAF_TSV) as fh:
-        assert len(fh.read()) == 592334
+        assert len(fh.read()) == 885143
 
 
 class TestLoadInput:
@@ -40,9 +39,9 @@ class TestLoadInput:
         assert all(isinstance(row, dict) for row in result)
         assert result[0] == {
             "chrom": "chr1",
-            "pos": "756268",
-            "name": "rs12567639",
-            "value": "0.308849",
+            "pos": "841085",
+            "name": "rs1574243",
+            "value": "0.0",
         }
 
     def test_load_input_tsv_with_header_and_comments(self):
@@ -54,8 +53,8 @@ class TestLoadInput:
             "start": "2488102",
             "stop": "2488174",
             "name": "TNFRSF14-chr1.0",
-            "MXX-ABCDT": "0",
-            "improper_pairs": "0",
+            "SXX-XXXT": "0.7692201318004654",
+            "improper_pairs": "16",
         }
 
     def test_load_input_vcf(self):
@@ -64,15 +63,15 @@ class TestLoadInput:
         assert all(isinstance(row, dict) for row in result)
         assert result[0] == {
             "CHROM": "chr1",
-            "POS": "19072316",
+            "POS": "2488139",
             "ID": ".",
-            "REF": "A",
-            "ALT": "T",
+            "REF": "G",
+            "ALT": "C",
             "QUAL": ".",
-            "FILTER": "low_depth;weak_evidence",
-            "INFO": "DP=1;MQ=124.00;FractionInformativeReads=1.000;AQ=28.36",
+            "FILTER": "weak_evidence",
+            "INFO": "DP=178;MQ=247.75;FractionInformativeReads=0.944;AQ=8.35",
             "FORMAT": "GT:SQ:AD:AF:F1R2:F2R1:DP:SB:MB",
-            "MXX-ABCDT": "0/1:0.86:0,1:1.0000:0,0:0,1:1:0,0,1,0:0,0,0,1",
+            "SXX-XXXT": "0/1:0.00:167,1:0.0060:75,1:92,0:168:78,89,1,0:84,83,1,0",
         }
 
 
@@ -89,7 +88,7 @@ class TestParseInput:
                 "FILTER": "low_depth;weak_evidence",
                 "INFO": "DP=1;MQ=124.00;FractionInformativeReads=1.000;AQ=28.36",
                 "FORMAT": "GT:SQ:AD:AF:F1R2:F2R1:DP:SB:MB",
-                "MXX-ABCDT": "0/1:0.86:0,1:1.0000:0,0:0,1:1:0,0,1,0:0,0,0,1",
+                "SXX-XXXT": "0/1:0.86:0,1:1.0000:0,0:0,1:1:0,0,1,0:0,0,0,1",
             },
             {
                 "CHROM": "chr3",
@@ -101,7 +100,7 @@ class TestParseInput:
                 "FILTER": "PASS",
                 "INFO": "DP=1;MQ=227.00;FractionInformativeReads=1.000;AQ=40.97;GermlineStatus=Germline_DB",
                 "FORMAT": "GT:SQ:AD:AF:F1R2:F2R1:DP:SB:MB",
-                "MXX-ABCDT": "0/1:4.69:0,1:1.0000:0,1:0,0:1:0,0,0,1:0,0,0,1",
+                "SXX-XXXT": "0/1:4.69:0,1:1.0000:0,1:0,0:1:0,0,0,1:0,0,0,1",
             },
         ]
         records = parse_vcf(expect)
@@ -115,7 +114,12 @@ class TestParseInput:
         rows = [
             {"chrom": "chr1", "pos": "100", "name": "rs1", "value": "0.5"},
             {"chrom": "2", "pos": "200", "name": "rs2", "value": "1.5"},
-            {"chrom": "chr3", "pos": "invalid", "name": "rs3", "value": "2.5"} # Should be skipped
+            {
+                "chrom": "chr3",
+                "pos": "invalid",
+                "name": "rs3",
+                "value": "2.5",
+            },  # Should be skipped
         ]
         records = parse_generic_tsv(rows)
         assert len(records) == 2
@@ -124,15 +128,33 @@ class TestParseInput:
 
     def test_parse_tn(self):
         rows = [
-            {"contig": "chr1", "start": "100", "stop": "200", "name": "geneA", "SAMPLE-1": "0.1", "improper_pairs": "0"},
-            {"contig": "2", "start": "300", "stop": "400", "name": "geneB", "SAMPLE-1": "0.2", "improper_pairs": "0"},
-            {"contig": "chr3", "start": "500", "stop": "600", "name": "geneC"} # Missing SAMPLE-1, should logically be skipped
+            {
+                "contig": "chr1",
+                "start": "100",
+                "stop": "200",
+                "name": "geneA",
+                "SAMPLE-1": "0.1",
+                "improper_pairs": "0",
+            },
+            {
+                "contig": "2",
+                "start": "300",
+                "stop": "400",
+                "name": "geneB",
+                "SAMPLE-1": "0.2",
+                "improper_pairs": "0",
+            },
+            {
+                "contig": "chr3",
+                "start": "500",
+                "stop": "600",
+                "name": "geneC",
+            },  # Missing SAMPLE-1, should logically be skipped
         ]
         records = parse_tn(rows, "SAMPLE-1")
         assert len(records) == 2
         assert records[0] == CNVRecord(contig="1", start=100, name="geneA", value=0.1)
         assert records[1] == CNVRecord(contig="2", start=300, name="geneB", value=0.2)
-
 
 
 # def test_correct_keys():
