0326c45fb6e52c91f6c31a47c6ff6e39cc70bed8
Dennis Dollée <no.ploidy@pm.me>
2026-04-01 17:26:27 +0200

    Move generate html logic to core + add outdir flag

--- src/spyCNV/cli.py
+++ src/spyCNV/cli.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
-from pathlib import Path
-from typing import Annotated
 
 import typer
 
-from spyCNV.core import render_html, write_file
-from spyCNV.io.loaders import create_cnv_data
+from spyCNV.core import generate_html
 
 app = typer.Typer(name="spyCNV", help="", add_completion=False)
 
@@ -14,26 +11,39 @@ app = typer.Typer(name="spyCNV", help="", add_completion=False)
 @app.command()
 def generate(
     sample_id: str = typer.Option(..., "--sample-id", "-s", help="Sample ID"),
-    vcf: str | None = typer.Option(None, "--vcf", "-v", help="Path to TSO500 hard-filtered VCF"),
-    tn: str | None = typer.Option(None, "--tn", "-t", help="Path to TSO500 tn.tsv.gz (LogRatio)"),
-    ballele: str | None = typer.Option(None, "--ballele", "-b", help="Path to HRD bAllele.tsv (BAF)"),
-    logratio: str | None = typer.Option(None, "--logratio", "-l", help="Path to HRD logRatio.tsv"),
-    segments: str | None = typer.Option(None, "--segments", help="Path to segments file (.seg)"),
+    vcf: str | None = typer.Option(
+        None, "--vcf", "-v", help="Path to TSO500 hard-filtered VCF"
+    ),
+    tn: str | None = typer.Option(
+        None, "--tn", "-t", help="Path to TSO500 tn.tsv.gz (LogRatio)"
+    ),
+    ballele: str | None = typer.Option(
+        None, "--ballele", "-b", help="Path to HRD bAllele.tsv (BAF)"
+    ),
+    logratio: str | None = typer.Option(
+        None, "--logratio", "-l", help="Path to HRD logRatio.tsv"
+    ),
+    segments: str | None = typer.Option(
+        None, "--segments", help="Path to segments file (.seg)"
+    ),
+    output_dir: str | None = typer.Option(
+        ".", "--output-dir", help="Path to output directory"
+    ),
 ):
     typer.echo("Generating HTML report...")
-    cnv_data = create_cnv_data(
+    if not output_dir:
+        output_dir = "."
+    generate_html(
         sample_id=sample_id,
-        tn_file=tn,
-        filtered_vcf=vcf,
-        logratio_file=logratio,
-        ballele_file=ballele,
-        segment_file=segments,
+        tn=tn,
+        vcf=vcf,
+        logratio=logratio,
+        ballele=ballele,
+        segments=segments,
+        output_path=output_dir,
     )
 
-    html_content = render_html(sample_id=sample_id, cnv_data=cnv_data)
-    out_file = Path(f"{sample_id}.html")
-    write_file(str(out_file), html_content)
-    typer.echo(f"Report written to {out_file}")
+    typer.echo(f"Report written successfully.")
 
 
 @app.command()
