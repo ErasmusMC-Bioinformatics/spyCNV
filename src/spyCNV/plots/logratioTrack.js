1ebc634fada001724882c7639d62d7c3aed2adba
Dennis Dollée <no.ploidy@pm.me>
2026-04-17 18:28:36 +0200

    Fix dynamic scaling + padding

--- src/spyCNV/plots/logratioTrack.js
+++ src/spyCNV/plots/logratioTrack.js
@@ -1,6 +1,8 @@
 const logratioTrack = (hrdData, tso500Data, segments, options = {}) => {
     let layers = [];
 
+    let clampMin = -3
+
     const xEncoding = {
         chrom: "contig",
         pos: "start",
@@ -17,24 +19,39 @@ const logratioTrack = (hrdData, tso500Data, segments, options = {}) => {
     const yEncoding = {
         field: "_clampedValue",
         type: "quantitative",
-        scale: { domain: [-6.3, 6.3] },
+        scale: { zero: false, padding: 0.02 },
         axis: { grid: true, title: "Log2", tickCount: 5 }
     };
 
     const clampOutliers = [
-        { type: "formula", expr: "datum.value > 6 ? 6 : (datum.value < -6 ? -6 : datum.value)", as: "_clampedValue" },
-        { type: "formula", expr: "datum.value > 6 || datum.value < -6 ? 'outlier' : 'normal'", as: "_outlierStatus" }
+        // { type: "formula", expr: `datum.value > ${clampMax} ? ${clampMax} : (datum.value < ${clampMin} ? ${clampMin} : datum.value)`, as: "_clampedValue" },
+        { type: "formula", expr: `(datum.value < ${clampMin} ? ${clampMin} : datum.value)`, as: "_clampedValue" },
+        // { type: "formula", expr: `datum.value > ${clampMax} || datum.value < ${clampMin} ? 'outlier' : 'normal'`, as: "_outlierStatus" }
+        { type: "formula", expr: `datum.value < ${clampMin} ? 'outlier' : 'normal'`, as: "_outlierStatus" }
     ]
+
+    // layers.push({
+    //     data: { values: [{ value: -3 }] },
+    //     mark: { type: "rule", clip: false, size: 1, opacity: 0.3 },
+    //     tooltip: false,
+    //     encoding: {
+    //         y: {
+    //             field: "value",
+    //             type: "quantitative"
+    //         },
+    //         color: { value: "#4575B4" }
+    //     }
+    // });
 
     if (hrdData) {
         layers.push({
             data: { name: "hrd_logratio" },
             transform: clampOutliers,
-            mark: { type: "point", geometricZoomBound: 12, opacity: { expr: "clamp(1 - zoomLevel * 0.1, 0.7, 1)" } },
+            // mark: { type: "point", clip: true, size: { "expr": "min(0.1 * pow(zoomLevel, 2), 120)" }, opacity: { expr: "clamp(1 - zoomLevel * 0.1, 0.7, 1)" } },
+            mark: { type: "point", clip: true, size: { "expr": "min(0.1 * pow(zoomLevel, 2), 120)" }, opacity: { expr: "clamp(1 - zoomLevel * 0.1, 0.7, 1)" } },
             encoding: {
                 x: xEncoding,
                 y: yEncoding,
-                size: { value: 120 },
                 color: { field: "_outlierStatus", type: "nominal", scale: { domain: ["normal", "outlier"], range: ["#8589ff", "red"] }, legend: null },
                 stroke: { field: "_outlierStatus", type: "nominal", scale: { domain: ["normal", "outlier"], range: ["#3c45e8", "darkred"] }, legend: null },
                 tooltip: [{ field: "contig", type: "nominal", title: "Chromosome" }, { field: "start", type: "quantitative", title: "Position" }, { field: "value", type: "quantitative", title: "Log2", format: ".3f" }]
@@ -46,11 +63,10 @@ const logratioTrack = (hrdData, tso500Data, segments, options = {}) => {
         layers.push({
             data: { name: "tso500_logratio" },
             transform: clampOutliers,
-            mark: { type: "point", geometricZoomBound: 12, opacity: { expr: "clamp(1 - zoomLevel * 0.1, 0.7, 1)" } },
+            mark: { type: "point", clip: true, size: { "expr": "min(0.1 * pow(zoomLevel, 2), 120)" }, opacity: { expr: "clamp(1 - zoomLevel * 0.1, 0.7, 1)" } },
             encoding: {
                 x: xEncoding,
                 y: yEncoding,
-                size: { value: 120 },
                 color: { field: "_outlierStatus", type: "nominal", scale: { domain: ["normal", "outlier"], range: ["#ffb14e", "red"] }, legend: null },
                 stroke: { field: "_outlierStatus", type: "nominal", scale: { domain: ["normal", "outlier"], range: ["#3c45e8", "darkred"] }, legend: null },
                 tooltip: [{ field: "contig", type: "nominal", title: "Chromosome" }, { field: "start", type: "quantitative", title: "Position" }, { field: "value", type: "quantitative", title: "Log2", format: ".3f" }]
@@ -62,9 +78,9 @@ const logratioTrack = (hrdData, tso500Data, segments, options = {}) => {
         layers.push({
             data: { values: segments, format: { type: "json" } },
             transform: [
-                { type: "formula", expr: "datum.value > 6 ? 6 : (datum.value < -6 ? -6 : datum.value)", as: "_clampedValue" }
+                { type: "formula", expr: `datum.value > 6 ? 6 : (datum.value < ${clampMin} ? ${clampMin} : datum.value)`, as: "_clampedValue" }
             ],
-            mark: { type: "rule", size: 3, tooltip: null },
+            mark: { type: "rule", clip: true, size: 3, tooltip: null },
             encoding: {
                 x: Object.assign({}, xEncoding, { pos: "start" }),
                 x2: { chrom: "contig", pos: "end", type: "locus" },
@@ -82,7 +98,7 @@ const logratioTrack = (hrdData, tso500Data, segments, options = {}) => {
 
     return {
         name: "logratioTrack",
-        height: options.height ?? 300,
+        height: options.height ?? 350,
         layer: layers,
         resolve: { scale: { y: "shared" } }
     };
