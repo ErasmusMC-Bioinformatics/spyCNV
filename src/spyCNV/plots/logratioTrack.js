dc2915a7c293f608317993a1969e81ee74614b98
Dennis Dollée <no.ploidy@pm.me>
2026-04-22 18:15:10 +0200

    Hide tooltip for minimum log value

--- src/spyCNV/plots/logratioTrack.js
+++ src/spyCNV/plots/logratioTrack.js
@@ -31,10 +31,9 @@ const logratioTrack = (hrdData, tso500Data, segments, options = {}) => {
     layers.push({
         data: { name: "min_logratio" },
         mark: { type: "rule", clip: true, size: 1, opacity: 0.3 },
-        tooltip: null,
         encoding: {
             y: {
-                field: "value",
+                field: "_value",
                 type: "quantitative"
             },
             color: { value: "#D73027" }
