657a84df627466dfd2e7ebd18f772527b8c1eecf
Dennis Dollée <no.ploidy@pm.me>
2026-06-18 16:34:33 +0200

    Add demo html

--- devenv.nix
+++ devenv.nix
@@ -23,15 +23,29 @@
     };
   };
 
-  scripts.dev = {
-    exec = ''
-      fd -tf | entr -c pytest
-    '';
-    description = "run pytest on src file change";
-    packages = with pkgs; [
-      fd
-      entr
-    ];
+  scripts = {
+    dev = {
+      description = "run pytest on src file change";
+      exec = ''
+        fd -tf | entr -c pytest
+      '';
+      packages = with pkgs; [
+        fd
+        entr
+      ];
+    };
+    gh-pages = {
+      description = "generate html from sample data for github pages";
+      exec = ''
+        spy generate --sample-id SXX-XXXT \
+          --vcf tests/data/SXX-XXXT.hard-filtered.vcf.gz \
+          --tn tests/data/SXX-XXXT.tn.tsv.gz \
+          --ballele tests/data/SXX-XXXT_bAllele.tsv \
+          --logratio tests/data/SXX-XXXT_logRatio.tsv \
+          --segments tests/data/SXX-XXXT.seg.called.merged \
+          --output-dir docs/ && mv docs/SXX-XXXT.spyCNV.html docs/index.html
+      '';
+    };
   };
 
   enterShell = ''
