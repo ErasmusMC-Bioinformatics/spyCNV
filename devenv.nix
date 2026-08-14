fdc996b90cf7abc73f4d2162199296f013b8faf2
Dennis Dollée <no.ploidy@pm.me>
2026-08-14 11:16:13 +0200

    Add script for live html output

--- devenv.nix
+++ devenv.nix
@@ -34,6 +34,18 @@
         entr
       ];
     };
+    dev-html = {
+      description = "generate an output file on src file change";
+      exec = ''
+        fd -tf | entr -c spy generate --sample-id SXX-XXXT \
+          --vcf tests/data/SXX-XXXT.hard-filtered.vcf.gz \
+          --tn tests/data/SXX-XXXT.tn.tsv.gz \
+          --ballele tests/data/SXX-XXXT_bAllele.tsv \
+          --logratio tests/data/SXX-XXXT_logRatio.tsv \
+          --segments tests/data/SXX-XXXT.seg.called.merged \
+          --output-dir /tmp/
+      '';
+    };
     gh-pages = {
       description = "generate html from sample data for github pages";
       exec = ''
